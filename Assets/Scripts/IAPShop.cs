9299aa50450e9111e6d6692e4f204ea606f1b92d
Komal Gupta <komal@tii.co.in>
2022-05-17 20:53:22 +0530

    consumed user apis

---
+++ Assets/Scripts/IAPShop.cs
@@ -0,0 +1,186 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Purchasing;
+using UnityEngine.Purchasing.Security;
+using System;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using System.Text;
+using TMPro;
+
+public class IAPShop : MonoBehaviour
+{
+    private string timePeriod = "com.techies.edugogy.onemonth";
+    private string subPeriod = "com.techies.edugogy.threemonth";
+
+    [SerializeField] public TextMeshProUGUI transactionId;
+
+    public class SubscriptionForm
+    {
+        public string transaction_id;
+
+        public string platform;
+
+        public string platform_plan_id;
+    }
+    private void Start()
+    {
+        //     StandardPurchasingModule.Instance().useFakeStoreAlways = true;
+        AddSubscriptionData(); // otherwise call on receipt validation and information received
+
+    }
+
+    void AddSubscriptionData() => StartCoroutine(AddSubscription_Coroutine());
+
+    IEnumerator AddSubscription_Coroutine()
+    {
+        SubscriptionForm subscriptionFormData = new SubscriptionForm { transaction_id = "14277687", platform = "apple", platform_plan_id = "com.techies.edugogy.onemonth" };
+        string json = JsonUtility.ToJson(subscriptionFormData);
+
+        Debug.Log(json);
+
+        byte[] bodyRaw = Encoding.UTF8.GetBytes(json);
+
+        string uri = "http://165.22.219.198/edugogy/api/v1/student-subscriptions";
+
+        var request = new UnityWebRequest(uri, "POST");
+
+        request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+        request.SetRequestHeader("Authorization", "Bearer 3VcmTskZ5jRINDiaO_489b0pdVsbTEy6");
+
+        yield return request.SendWebRequest();
+
+        if (request.result != UnityWebRequest.Result.Success)
+        {
+            Debug.Log("Error: " + request.error);
+        }
+        else
+        {
+            Debug.Log(request.result);
+            Debug.Log(request.downloadHandler.text);
+
+            // {"transaction_id":"14277687","platform":"apple","platform_plan_id":"com.techies.edugogy.onemonth","student_id":3,"age_group_id":2,"plan_id":2,"plan_title":"1 Month","plan_term":"30 day","number_of_level":30,"start_at":1655356249,"expire_at":1657948249,"created_at":1652797078,"updated_at":1652797078,"id":4}
+        }
+    }
+
+    public void OnPurchaseComplete(Product product)
+    {
+        if (product.definition.id == timePeriod)
+        {
+            Debug.Log("Subscription successful for one month");
+
+        }
+
+        if (product.definition.id == subPeriod)
+        {
+            Debug.Log("Subscription successful for three month");
+        }
+    }
+
+    public void OnPurchaseFailed(Product product, PurchaseFailureReason reason)
+    {
+        Debug.Log("Purchase of " + product.definition.id + " failed due to" + reason);
+    }
+
+    public PurchaseProcessingResult ProcessPurchase(PurchaseEventArgs e)
+    {
+        bool validPurchase = true;
+        var validator = new CrossPlatformValidator(GooglePlayTangle.Data(),
+                AppleTangle.Data(), Application.identifier);
+        Debug.Log("Checking for tangle data under purchase process");
+        Debug.Log(validator + " value of validator");
+
+        try
+        {
+            // On Google Play, result has a single product ID.
+            // On Apple stores, receipts contain multiple products.
+            var result = validator.Validate(e.purchasedProduct.receipt);
+            // For informational purposes, we list the receipt(s)
+            Debug.Log("Receipt is valid. Contents:");
+            foreach (IPurchaseReceipt productReceipt in result)
+            {
+                Debug.Log(productReceipt.productID);
+                Debug.Log(productReceipt.purchaseDate);
+                Debug.Log(productReceipt.transactionID);
+
+                GooglePlayReceipt google = productReceipt as GooglePlayReceipt;
+                if (null != google)
+                {
+                    // This is Google's Order ID.
+                    // Note that it is null when testing in the sandbox
+                    // because Google's sandbox does not provide Order IDs.
+                    Debug.Log(google.transactionID);
+                    Debug.Log(google.purchaseDate);
+                    Debug.Log(google.purchaseState);
+                    Debug.Log(google.purchaseToken);
+                    transactionId.GetComponent<TextMeshProUGUI>().text = google.transactionID;
+
+                }
+
+                AppleInAppPurchaseReceipt apple = productReceipt as AppleInAppPurchaseReceipt;
+                if (null != apple)
+                {
+                    Debug.Log(apple.transactionID);
+                    Debug.Log(apple.purchaseDate);
+                    Debug.Log(apple.originalTransactionIdentifier);
+                    Debug.Log(apple.subscriptionExpirationDate);
+                    Debug.Log(apple.cancellationDate);
+                    Debug.Log(apple.quantity);
+                    transactionId.GetComponent<TextMeshProUGUI>().text = apple.transactionID;
+
+                }
+            }
+
+        }
+        catch (IAPSecurityException)
+        {
+            Debug.Log("Invalid receipt, not unlocking content");
+            validPurchase = false;
+        }
+
+
+
+        return PurchaseProcessingResult.Complete;
+
+
+
+        // Unlock the appropriate content here.
+
+        // if (e.purchasedProduct.receipt != null)
+        // {
+        //     if (e.purchasedProduct.definition.type == ProductType.Subscription)
+        //     {
+        //         // if (checkIfProductIsAvailableForSubscriptionManager(e.purchasedProduct.receipt))
+        //         // {
+        //         string intro_json = (introductory_info_dict == null || !introductory_info_dict.ContainsKey(item.definition.storeSpecificId)) ? null : introductory_info_dict[item.definition.storeSpecificId];
+        //         SubscriptionManager p = new SubscriptionManager(e.purchasedProduct, intro_json);
+        //         SubscriptionInfo info = p.getSubscriptionInfo();
+        //         Debug.Log("product id is: " + info.getProductId());
+        //         Debug.Log("purchase date is: " + info.getPurchaseDate());
+        //         Debug.Log("subscription next billing date is: " + info.getExpireDate());
+        //         Debug.Log("is subscribed? " + info.isSubscribed().ToString());
+        //         Debug.Log("is expired? " + info.isExpired().ToString());
+        //         Debug.Log("is cancelled? " + info.isCancelled());
+        //         Debug.Log("product is in free trial peroid? " + info.isFreeTrial());
+        //         Debug.Log("product is auto renewing? " + info.isAutoRenewing());
+        //         Debug.Log("subscription remaining valid time until next billing date is: " + info.getRemainingTime());
+        //         Debug.Log("is this product in introductory price period? " + info.isIntroductoryPricePeriod());
+        //         Debug.Log("the product introductory localized price is: " + info.getIntroductoryPrice());
+        //         Debug.Log("the product introductory price period is: " + info.getIntroductoryPricePeriod());
+        //         Debug.Log("the number of product introductory price period cycles is: " + info.getIntroductoryPricePeriodCycles());
+        //         // }
+        //         // else
+        //         // {
+        //         //     Debug.Log("This product is not available for SubscriptionManager class, only products that are purchase by 1.19+ SDK can use this class.");
+        //         // }
+        //     }
+        // }
+
+
+
+
+    }
+}
---
+++ Assets/Scripts/MissionManagement.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using System;
+using System.Text;
+
+public class MissionManagement : MonoBehaviour
+{
+    public class MissionForm
+    {
+        public int day_level_id;
+    }
+    // Start is called before the first frame update
+    void Awake()
+    {
+        // StartMission();
+        GetNewWordData();
+    }
+
+    void StartMission() => StartCoroutine(StartMission_Coroutine());
+
+    void GetNewWordData() => StartCoroutine(GetNounData_Coroutine());
+
+
+    IEnumerator StartMission_Coroutine()
+    {
+        MissionForm missionFormData = new MissionForm { day_level_id = 4 };
+        string json = JsonUtility.ToJson(missionFormData);
+
+        Debug.Log(json);
+
+        byte[] bodyRaw = Encoding.UTF8.GetBytes(json);
+
+        string uri = "http://165.22.219.198/edugogy/api/v1/student-levels/start-level";
+
+        var request = new UnityWebRequest(uri, "POST");
+
+        request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+        request.SetRequestHeader("Authorization", "Bearer 3VcmTskZ5jRINDiaO_489b0pdVsbTEy6");
+
+        yield return request.SendWebRequest();
+
+        if (request.result != UnityWebRequest.Result.Success)
+        {
+            Debug.Log("Error: " + request.error);
+        }
+        else
+        {
+            Debug.Log(request.result);
+            Debug.Log(request.downloadHandler.text);
+
+        }
+    }
+
+    IEnumerator GetNounData_Coroutine()
+    {
+
+        string uri = "http://165.22.219.198/edugogy/api/v1//day-levels/1?expand=newWords";
+
+        var request = new UnityWebRequest(uri, "GET");
+
+        request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+        request.SetRequestHeader("Authorization", "Bearer 3VcmTskZ5jRINDiaO_489b0pdVsbTEy6");
+
+        yield return request.SendWebRequest();
+
+        if (request.result != UnityWebRequest.Result.Success)
+        {
+            Debug.Log("Error: " + request.error);
+        }
+        else
+        {
+            Debug.Log(request.result);
+            Debug.Log(request.downloadHandler.text);
+
+
+        }
+    }
+
+}
