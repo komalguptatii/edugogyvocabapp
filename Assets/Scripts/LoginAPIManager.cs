c12c9d58927f2426344eb8dd9d72e9c3fc6f01e9
Komal Gupta <komal@tii.co.in>
2022-05-27 06:31:11 +0530

    Added verify otp functionality

--- Assets/Scripts/LoginAPIManager.cs
+++ Assets/Scripts/LoginAPIManager.cs
@@ -6,15 +6,21 @@ using UnityEngine.Networking;
 using System;
 using System.Text;
 using TMPro;
-// using UnityEditor;
-// using JsonUtility;
+using UnityEngine.SceneManagement;
 
 public class LoginAPIManager : MonoBehaviour
 {
     public TMP_Dropdown countryCodeDropdown;
     
+    public TMP_InputField phoneNumberInput;
 
-[Serializable]
+    public GameObject fbBtn;
+    public GameObject appleBtn;
+    public GameObject googleBtn; 
+
+    public int selectedCountryCode;
+
+    [Serializable]
     public class CountryCodeData
     {
         public int id ;
@@ -24,21 +30,92 @@ public class LoginAPIManager : MonoBehaviour
         public string dial_code;
     }
 
- [Serializable]
+    [Serializable]
     public class CountryCodeList {
  
-    public CountryCodeData[] items;
-}
+         public CountryCodeData[] items;
+    }
+    
+    [Serializable]
+    public class LoginForm
+    {
+        public string phone;
+        public int country_code_id;
+    }
 
-         CountryCodeList listObject = new CountryCodeList();
+    [Serializable]
+    public class ValidateOTPForm
+    {
+        public string phone;
+        public int country_code_id;
+        public int otp;
+    }
 
-    public int selectedCountryCode;
+    [Serializable]
+    public class SignedUpUser
+    {
+        public string phone;
+        public int id;
+        public int country_code_id;
+        public int is_existing_user;
+       
+    }
+
+    // "student": {
+    //     "id": 3,
+    //     "name": "Komal Gupta",
+    //     "phone": "9855940600",
+    //     "age_group_id": 2,
+    //     "country_code_id": 88
+    // }
+
+    [Serializable]
+    public class Student
+    {
+        public int id;
+        public string name;
+        public string phone;
+        public int age_group_id;
+        public int country_code_id;
+    }
+
+    [Serializable]
+    public class ResendOTP
+    {
+        public string phone;
+        public int country_code_id;
+        public int student_id;
+        public Student student;
+    }
+
+    CountryCodeList listObject = new CountryCodeList();
+
+     public string countryListjson;
+
     string fixJson(string value)            // Added object type to JSON
     {
         value = "{\"items\":" + value + "}";
         return value;
     }
 
+
+    void Awake()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            Debug.Log("will hide apple sign in button here later");
+
+        }
+        //change position of facebook and google button after hiding apple button
+        appleBtn.SetActive(false);
+        ChangeButtonPosition();
+    }
+
+    void Start()
+    {
+        //
+        GetCountryCodeListData();
+    }
 
     IEnumerator GetCountryCodeListData_Coroutine()
     {
@@ -70,7 +147,7 @@ public class LoginAPIManager : MonoBehaviour
                 //  Debug.Log(dropdownOptions);
                 // m_DropOptions.Add((listObject.items[i].id).ToString());
                 // Debug.Log(m_DropOptions);
-            countryCodeDropdown.options.Add (new TMP_Dropdown.OptionData() {text = listObject.items[i].dial_code});
+                countryCodeDropdown.options.Add (new TMP_Dropdown.OptionData() {text = listObject.items[i].dial_code});
             }
         }
     }
@@ -81,30 +158,17 @@ public class LoginAPIManager : MonoBehaviour
         Debug.Log(selectedCountryCode);
     }
 
-    [Serializable]
-    public class LoginForm
+    
+
+    private void ChangeButtonPosition()
     {
-        public string phone;
-        public int country_code_id;
-    }
+        Vector3 fbPos = fbBtn.transform.position;
+        fbPos.x += 100f;
+        fbBtn.transform.position = fbPos;
 
-    [Serializable]
-    public class ValidateOTPForm
-    {
-        public string phone;
-        public int country_code_id;
-        public int otp;
-    }
-
-    public string countryListjson;
-
-
-    void Start()
-    {
-        // countryCodeData = new CountryCodeData();
-        GetCountryCodeListData();
-
-        // resendOTPRequest();
+        Vector3 googlePos = googleBtn.transform.position;
+        googlePos.x -= 100f;
+        googleBtn.transform.position = googlePos;
     }
 
     void GetCountryCodeListData() => StartCoroutine(GetCountryCodeListData_Coroutine());
@@ -120,13 +184,14 @@ public class LoginAPIManager : MonoBehaviour
 
     IEnumerator ProcessLoginRequest_Coroutine()  // Actually this is API to sign up
     {
-        // "9855940600", 88 - country code id not code
-        LoginForm loginFormData = new LoginForm { phone = "9855940600", country_code_id = selectedCountryCode };
+        SignedUpUser userSignUp = new SignedUpUser();
+        //Validation for Login via mobile number
+        if (selectedCountryCode != 0 && phoneNumberInput.text != ""){
+            // "9855940600", 88 - country code id not code
+        LoginForm loginFormData = new LoginForm { phone = phoneNumberInput.text, country_code_id = selectedCountryCode };
         string json = JsonUtility.ToJson(loginFormData);
 
         Debug.Log(json);
-
-
         string uri = "http://165.22.219.198/edugogy/api/v1/students";
 
         byte[] bodyRaw = Encoding.UTF8.GetBytes(json);
@@ -135,27 +200,53 @@ public class LoginAPIManager : MonoBehaviour
 
         request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
         request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
 
         yield return request.SendWebRequest();
 
         if (request.error != null)
         {
             Debug.Log("Error: " + request.error);
+            if (request.responseCode == 422)
+            {
+             Debug.Log(request.downloadHandler.text);
+// {"error":[{"code":0,"source":"phone","title":"Phone \"9855940600\" has already been taken.","detail":"Phone \"9855940600\" has already been taken."}]}
+                resendOTPRequest();
+            }
         }
         else
         {
-            Debug.Log("All OK");
             Debug.Log("Status Code: " + request.responseCode);
+            
             Debug.Log(request.downloadHandler.text);
+            
+            string userJson = request.downloadHandler.text;
+            string jsonString = fixJson(userJson);
+            Debug.Log(jsonString);
+            userSignUp = JsonUtility.FromJson<SignedUpUser>(jsonString);
 
+            Debug.Log(userSignUp);
+            if (userSignUp.is_existing_user == 0)
+            {
+                resendOTPRequest();
+            }
 
+            // MoveToVerifyOTP();
             // "phone": "9855940600",
             // "country_code_id": 88,
             // "id": 3,
             // "is_existing_user": 0
+            // { "auth_key":"3VcmTskZ5jRINDiaO_489b0pdVsbTEy6"}
 
         }
+        }
+    }
 
+   
+
+     void MoveToVerifyOTP()
+    {
+        SceneManager.LoadScene("VerifyOTP");
     }
 
     IEnumerator ProcessValidateOTPRequest_Coroutine()  //validate otp
@@ -197,11 +288,11 @@ public class LoginAPIManager : MonoBehaviour
     IEnumerator ProcessResendMobileOTPRequest_Coroutine()  //Resend validate otp, also used for login
     {
 
-        ValidateOTPForm validateOTPFormData = new ValidateOTPForm { phone = "9855940600", country_code_id = 88 };
+        ResendOTP resendOTP = new ResendOTP();
+        ValidateOTPForm validateOTPFormData = new ValidateOTPForm { phone = phoneNumberInput.text, country_code_id = selectedCountryCode };
         string json = JsonUtility.ToJson(validateOTPFormData);
 
         Debug.Log(json);
-
 
         string uri = "http://165.22.219.198/edugogy/api/v1/students/resend-otp";
 
@@ -226,10 +317,26 @@ public class LoginAPIManager : MonoBehaviour
             Debug.Log(request.result);
             Debug.Log(request.downloadHandler.text);
 
+            string resendOTPJson = request.downloadHandler.text;
+            // string jsonString = fixJson(userJson);
+            // Debug.Log(jsonString);
+            resendOTP = JsonUtility.FromJson<ResendOTP>(resendOTPJson);
+
+            Debug.Log(resendOTP.phone);
+            Debug.Log(resendOTP.student.name);
+            SavePhoneNumber(resendOTP.phone);
+            MoveToVerifyOTP();
+            //save phone number
 
         }
 
     }
 
 
+ void SavePhoneNumber(string phone){
+        PlayerPrefs.SetString("phone", phone);
+        PlayerPrefs.SetInt("countryCodeId", selectedCountryCode);
+    }
+
+     
 }
---
+++ Assets/Scripts/VerifyOTPManager.cs
@@ -0,0 +1,240 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using System;
+using System.Text;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class VerifyOTPManager : MonoBehaviour
+{
+        public TMP_InputField firstDigit;
+    public TMP_InputField secondDigit;
+    public TMP_InputField thirdDigit;
+    public TMP_InputField fourthDigit;
+
+    string phoneNumber;
+    int countryCodeId;
+    public string otpEntered;
+
+    [Serializable]
+    public class ValidateOTPForm
+    {
+        public string phone;
+        public int country_code_id;
+        public int otp;
+    }
+
+    [Serializable]
+    public class GetAuthKey
+    {
+        public string auth_key;
+    }
+
+    [Serializable]
+    public class Student
+    {
+        public int id;
+        public string name;
+        public string phone;
+        public int age_group_id;
+        public int country_code_id;
+    }
+
+    [Serializable]
+    public class ResendOTP
+    {
+        public string phone;
+        public int country_code_id;
+        public int student_id;
+        public Student student;
+    }
+
+    string fixJson(string value)            // Added object type to JSON
+    {
+        value = "{\"items\":" + value + "}";
+        return value;
+    }
+
+
+    void Awake()
+    {
+        SetInputCharacterLength();
+        if (PlayerPrefs.HasKey("phone"))
+        {
+            phoneNumber = PlayerPrefs.GetString("phone");
+            countryCodeId = PlayerPrefs.GetInt("countryCodeId");
+            Debug.Log(phoneNumber);
+            Debug.Log(countryCodeId);
+
+        }
+
+    }
+
+    void SetInputCharacterLength()
+    {
+        firstDigit.characterLimit = 1;
+        secondDigit.characterLimit = 1;
+        thirdDigit.characterLimit = 1;
+        fourthDigit.characterLimit = 1;
+
+        firstDigit.Select();
+
+    }
+
+    public void OnTextValueChanged(string data)
+    {
+        Debug.Log(firstDigit.text);
+        Debug.Log(data);
+        if (firstDigit.text.Length == 1)
+        {
+            secondDigit.Select();
+            secondDigit.ActivateInputField();
+            otpEntered = (firstDigit.text).ToString();
+        }
+
+       
+
+
+         
+    }
+
+
+    public void secondDigitValueChanged(string data)
+    {
+         if (secondDigit.text.Length == 1)
+        {
+            thirdDigit.Select();
+            thirdDigit.ActivateInputField();
+            otpEntered = otpEntered + (secondDigit.text).ToString();
+        }
+    }
+
+    public void thirdDigitValueChanged(string data)
+    {
+        
+        if (thirdDigit.text.Length == 1)
+        {
+            fourthDigit.Select();
+            fourthDigit.ActivateInputField();
+            otpEntered = otpEntered + (thirdDigit.text).ToString();
+        }
+    }
+
+    public void fourthDigitValueChanged(string data)
+    {
+       if (fourthDigit.text.Length == 1)
+        {
+            
+            otpEntered = otpEntered + (fourthDigit.text).ToString();
+            Debug.Log(otpEntered);
+        }
+    }
+
+    public void validateOTPRequest() => StartCoroutine(ProcessValidateOTPRequest_Coroutine());
+
+    public void resendOTPRequest() => StartCoroutine(ProcessResendMobileOTPRequest_Coroutine());
+  
+
+    void SaveAuthKey(string auth_key){
+        PlayerPrefs.SetString("auth_key", "Bearer " + auth_key);
+    }
+
+     void MoveToSubscription()
+    {
+        SceneManager.LoadScene("IAPCatalog");
+    }
+
+    IEnumerator ProcessValidateOTPRequest_Coroutine()  //validate otp
+    {
+        var otp = int.Parse(otpEntered);
+        Debug.Log(otp);
+        GetAuthKey getKey = new GetAuthKey();
+        
+        ValidateOTPForm validateOTPFormData = new ValidateOTPForm { phone = phoneNumber, country_code_id = countryCodeId, otp = otp };
+        string json = JsonUtility.ToJson(validateOTPFormData);
+
+        Debug.Log(json);
+
+
+        string uri = "http://165.22.219.198/edugogy/api/v1/students/validate-otp";
+
+        byte[] bodyRaw = Encoding.UTF8.GetBytes(json);
+
+        var request = new UnityWebRequest(uri, "POST");
+
+        request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+
+        yield return request.SendWebRequest();
+
+        if (request.error != null)
+        {
+            Debug.Log("Error: " + request.error);
+        }
+        else
+        {
+            Debug.Log("All OK");
+            Debug.Log("Status Code: " + request.responseCode);
+            Debug.Log(request.result);
+            Debug.Log(request.downloadHandler.text);
+            // { "auth_key":"zJMqRh3THK3WTaTh1x7CAMO1T3s0Cxes"}
+
+             string validateOTPJson = request.downloadHandler.text;
+
+            getKey = JsonUtility.FromJson<GetAuthKey>(validateOTPJson);
+            Debug.Log(getKey.auth_key);
+            SaveAuthKey(getKey.auth_key);
+            MoveToSubscription();
+        }
+
+    }
+
+    IEnumerator ProcessResendMobileOTPRequest_Coroutine()  //Resend validate otp, also used for login
+    {
+
+        ResendOTP resendOTP = new ResendOTP();
+        ValidateOTPForm validateOTPFormData = new ValidateOTPForm { phone = phoneNumber, country_code_id = countryCodeId };
+        string json = JsonUtility.ToJson(validateOTPFormData);
+
+        Debug.Log(json);
+
+        string uri = "http://165.22.219.198/edugogy/api/v1/students/resend-otp";
+
+        byte[] bodyRaw = Encoding.UTF8.GetBytes(json);
+
+        var request = new UnityWebRequest(uri, "POST");
+
+        request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+
+        yield return request.SendWebRequest();
+
+        if (request.error != null)
+        {
+            Debug.Log("Error: " + request.error);
+        }
+        else
+        {
+            Debug.Log("All OK");
+            Debug.Log("Status Code: " + request.responseCode);
+            Debug.Log(request.result);
+            Debug.Log(request.downloadHandler.text);
+
+            string resendOTPJson = request.downloadHandler.text;
+            // string jsonString = fixJson(userJson);
+            // Debug.Log(jsonString);
+            resendOTP = JsonUtility.FromJson<ResendOTP>(resendOTPJson);
+            Debug.Log(resendOTP.phone);
+            Debug.Log(resendOTP.student.name);
+           
+        }
+
+    }
+
+
+}
