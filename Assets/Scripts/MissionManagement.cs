a893db537a91c7d79b993be79d3648efea96eb7a
Komal Gupta <komal@tii.co.in>
2022-06-03 06:05:04 +0530

    Added all content based screens

--- Assets/Scripts/MissionManagement.cs
+++ Assets/Scripts/MissionManagement.cs
@@ -19,16 +19,96 @@ public class MissionManagement : MonoBehaviour
     }
 
     [SerializeField]
+    public TextMeshProUGUI title;
+
+    [SerializeField]
     public TextMeshProUGUI newWord;
 
     [SerializeField]
+    public Button speakerButton;
+
+//Noun
+ [SerializeField]
     public TextMeshProUGUI meaningAsNoun;
 
     [SerializeField]
     public TextMeshProUGUI sentenceOfNoun;
 
+    //Verb
     [SerializeField]
-    public Button speakerButton;
+    public TextMeshProUGUI meaningAsVerb;
+
+    [SerializeField]
+    public TextMeshProUGUI sentenceOfVerb;
+
+
+   //Adverb
+    [SerializeField]
+    public TextMeshProUGUI meaningAsAdverb;
+
+    [SerializeField]
+    public TextMeshProUGUI sentenceOfAdverb;
+
+       //Adjective
+    [SerializeField]
+    public TextMeshProUGUI meaningAsAdjective;
+
+    [SerializeField]
+    public TextMeshProUGUI sentenceOfAdjective;
+
+    //Conversation
+    [SerializeField]
+    public TextMeshProUGUI convoDescription;
+
+
+      //Anotherwayofusingword
+    [SerializeField]
+    public TextMeshProUGUI meaningAnotherwayofusingword;
+
+    [SerializeField]
+    public TextMeshProUGUI sentenceAnotherwayofusingword;
+
+    [SerializeField]
+    public TextMeshProUGUI sentenceOfDUT;
+
+ //Idiom
+    [SerializeField]
+    public TextMeshProUGUI meaningAsIdiom;
+
+    [SerializeField]
+    public TextMeshProUGUI sentenceOfIdiom;
+
+     //multiple words
+    [SerializeField]
+    public TextMeshProUGUI placeholderMultipleWords;
+
+    [SerializeField]
+    public TextMeshProUGUI sentenceOfMultipleWords;
+
+    //Synonym
+    [SerializeField]
+    public TextMeshProUGUI synonymTitle;
+
+    [SerializeField]
+    public TextMeshProUGUI wordOfSynonym;
+
+    [SerializeField]
+    public TextMeshProUGUI meaningAsSynonym;
+
+    [SerializeField]
+    public TextMeshProUGUI sentenceOfSynonym;
+
+    //Antonym
+    [SerializeField]
+    public TextMeshProUGUI antonymTitle;
+
+    [SerializeField]
+    public TextMeshProUGUI wordOfAntonym;
+    [SerializeField]
+    public TextMeshProUGUI meaningAsAntonym;
+
+    [SerializeField]
+    public TextMeshProUGUI sentenceOfAntonym;
 
     [SerializeField]
     public Image singleSentenceNounBoard;
@@ -53,8 +133,9 @@ public class MissionManagement : MonoBehaviour
     string auth_key;
     int dayLevelId;
 
-    int nextNumber = 0;
-    int backNumber = 0;
+    int nextNumber = 1;
+
+    int totalNumber = 11;
 
     [Serializable]
      public class AllDetail
@@ -83,7 +164,7 @@ public class MissionManagement : MonoBehaviour
         public Verb[] verbs;
         public Noun[] nouns;
         public Adverb[] adverbs;
-        public Adjective adjectives;
+        public Adjective[] adjectives;
         public DailyUseTip[] dailyUseTips;
         public OtherWayUsingWord[] otherWayUsingWords;
         public Idiom[] idioms;
@@ -319,6 +400,11 @@ public class MissionManagement : MonoBehaviour
     {
         // StartMission(); // start mission after fetching id from detail all api
 
+        singleSentenceNounBoard.gameObject.SetActive(false);
+        multipleSentenceNounBoard.gameObject.SetActive(false);
+
+        
+
          if (PlayerPrefs.HasKey("auth_key"))
         {
             auth_key = PlayerPrefs.GetString("auth_key");
@@ -386,12 +472,12 @@ IEnumerator DownloadImage(string mediaUrl)
             Debug.Log(request.downloadHandler.text);
 
             string jsonString = request.downloadHandler.text;
-            // var details = JSON.Parse(jsonString);
-            // Debug.Log(details["id"]);
-            // string detailJson = fixJson(jsonString);
+           
             allDetailData = JsonUtility.FromJson<AllDetail>(jsonString);
             dayLevelId = allDetailData.id;
             Debug.Log(allDetailData.conversation.id);
+            NewWordSetup();
+
             StartMission();            
         }
     }
@@ -424,7 +510,8 @@ IEnumerator DownloadImage(string mediaUrl)
         {
             Debug.Log(request.result);
             Debug.Log(request.downloadHandler.text);
-            GetNounDetails();
+            // GetNounDetails();
+            NewWordSetup();
 
         }
     }
@@ -464,17 +551,13 @@ IEnumerator DownloadImage(string mediaUrl)
     {
          if (allDetailData.newWords.Length == 1)
             {
-                Debug.Log("Noun data");
-                Debug.Log(allDetailData.newWords[0].name);
 
-                Debug.Log(allDetailData.newWords[0].id);
                 newWord.text = allDetailData.newWords[0].name;
                 StartCoroutine(DownloadImage(allDetailData.newWords[0].image_url));
-                Debug.Log("Check length of noun");
-                Debug.Log(allDetailData.newWords[0].nouns.Length);
+            
                 NewWord newWordDetails = new NewWord();
                 newWordDetails = allDetailData.newWords[0];
-                Debug.Log(newWordDetails.nouns[0].description);
+                title.text = "Noun";
                 Noun newNoun = new Noun();
                 newNoun = newWordDetails.nouns[0];
                 meaningAsNoun.text = newNoun.description;
@@ -482,43 +565,298 @@ IEnumerator DownloadImage(string mediaUrl)
                 {
                     for (var i = 0; i < newNoun.nounSentences.Length; i++)
                     {
-                        singleSentenceNounBoard.enabled = false;
-                        multipleSentenceNounBoard.enabled = true;
+                        singleSentenceNounBoard.gameObject.SetActive(false);
+                        multipleSentenceNounBoard.gameObject.SetActive(true);
                         Vector2 prefabPosition = sentencePrefab.transform.position;
                         RectTransform rt = (RectTransform)sentencePrefab.transform;
                         var height = rt.rect.height;
-                        var calculatedHeight = prefabPosition.y - height - 27f;
+                        var calculatedHeight = prefabPosition.y - 164f;
+                        
                         GameObject secondSentencePrefab = Instantiate(sentencePrefab).gameObject;
                         secondSentencePrefab.transform.position = new Vector2(prefabPosition.x, calculatedHeight);
                         secondSentencePrefab.transform.SetParent(parent, true);
                         noOfSentences[i] = secondSentencePrefab;
                         GameObject childObj = secondSentencePrefab.transform.GetChild(0).gameObject;
                         TMPro.TMP_Text mytext = childObj.GetComponent<TMPro.TMP_Text>();
-                        mytext.text = newNoun.nounSentences[i].description;
+                        mytext.text = newNoun.nounSentences[0].description;
+                        SetBottomTitleLabel();
                     }
                 }
                 else
                 {
-                    singleSentenceNounBoard.enabled = true;
-                    multipleSentenceNounBoard.enabled = false;
+                    singleSentenceNounBoard.gameObject.SetActive(true);
+                    multipleSentenceNounBoard.gameObject.SetActive(false);
                     sentenceOfNoun.text = newNoun.nounSentences[0].description;
                 }
             }
     }
 
-    public void NextButton()
+    public void VerbSetup()
     {
-        nextNumber = nextNumber + 1;
+         if (allDetailData.newWords.Length == 1)
+            {
 
+                newWord.text = allDetailData.newWords[0].name;
+                StartCoroutine(DownloadImage(allDetailData.newWords[0].image_url));
+            
+                NewWord newWordDetails = new NewWord();
+                newWordDetails = allDetailData.newWords[0];
+                title.text = "Verb";
+                Verb verbDetail = new Verb();
+                verbDetail = newWordDetails.verbs[0];
+                meaningAsVerb.text = verbDetail.description;
+                
+                sentenceOfVerb.text = verbDetail.verbSentences[0].description;
+            }
+    }
+    
+    public void AdverbSetup()
+    {
+         if (allDetailData.newWords.Length == 1)
+            {
+
+                newWord.text = allDetailData.newWords[0].name;
+                StartCoroutine(DownloadImage(allDetailData.newWords[0].image_url));
+            
+                NewWord newWordDetails = new NewWord();
+                newWordDetails = allDetailData.newWords[0];
+                title.text = "Adverb";
+                Adverb adverbDetail = new Adverb();
+                adverbDetail = newWordDetails.adverbs[0];
+                meaningAsAdverb.text = adverbDetail.description;
+                
+                sentenceOfAdverb.text = adverbDetail.adverbSentences[0].description;
+            }
     }
 
-    public void BackButton()
+    public void AdjectiveSetup()
     {
-        if (backNumber > 0)
-        {
-        backNumber = backNumber - 1;
+         if (allDetailData.newWords.Length == 1)
+            {
 
+                newWord.text = allDetailData.newWords[0].name;
+                StartCoroutine(DownloadImage(allDetailData.newWords[0].image_url));
+            
+                NewWord newWordDetails = new NewWord();
+                newWordDetails = allDetailData.newWords[0];
+                title.text = "Adjective";
+                Adjective adjectiveDetail = new Adjective();
+                adjectiveDetail = newWordDetails.adjectives[0];
+                meaningAsAdjective.text = adjectiveDetail.description;
+                
+                sentenceOfAdjective.text = adjectiveDetail.adjectiveSentences[0].description;
+            }
+    }
+
+    public void ConversationSetup()
+    {
+         if (allDetailData.newWords.Length == 1)
+            {            
+                
+                Conversation convo = new Conversation();
+                convo = allDetailData.conversation;
+                Debug.Log(convo.description);
+                convoDescription.text = convo.description;
+                
+            }
+    }
+
+
+    public void DailyTipsSetup()
+    {
+         if (allDetailData.newWords.Length == 1)
+            {
+
+                newWord.text = allDetailData.newWords[0].name;
+                // StartCoroutine(DownloadImage(allDetailData.newWords[0].image_url));
+            
+                NewWord newWordDetails = new NewWord();
+                newWordDetails = allDetailData.newWords[0];
+                title.text = "Daily Use Tips";
+                DailyUseTip dailyUseTipsDetails = new DailyUseTip();
+                dailyUseTipsDetails = newWordDetails.dailyUseTips[0];
+                
+                sentenceOfDUT.text = dailyUseTipsDetails.description;
+
+    
+            }
+    }
+
+    public void AnotherWayOfUsingWordSetup()
+    {
+        if (allDetailData.newWords.Length == 1)
+            {
+
+                newWord.text = allDetailData.newWords[0].name;
+            
+                NewWord newWordDetails = new NewWord();
+                newWordDetails = allDetailData.newWords[0];
+                // title.text = "Another Way of using word";
+                OtherWayUsingWord anotherwayDetail = new OtherWayUsingWord();
+                anotherwayDetail = newWordDetails.otherWayUsingWords[0];
+                meaningAnotherwayofusingword.text = anotherwayDetail.description;
+                
+                sentenceAnotherwayofusingword.text = anotherwayDetail.otherWayUsingWordSentences[0].description;
+            }
+    }
+
+    public void IdiomSetup()
+    {
+        if (allDetailData.newWords.Length == 1)
+            {
+
+                newWord.text = allDetailData.newWords[0].name;
+            
+                NewWord newWordDetails = new NewWord();
+                newWordDetails = allDetailData.newWords[0];
+                // title.text = "Another Way of using word";
+                Idiom idiomDetails = new Idiom();
+                idiomDetails = newWordDetails.idioms[0];
+                meaningAsIdiom.text = idiomDetails.description;
+                
+                sentenceOfIdiom.text = idiomDetails.idiomSentences[0].description;
+            }
+    }
+
+    public void MultipleWordSetup()
+    {
+        if (allDetailData.newWords.Length == 1)
+            {
+
+                newWord.text = allDetailData.newWords[0].name;
+            
+                NewWord newWordDetails = new NewWord();
+                newWordDetails = allDetailData.newWords[0];
+                // title.text = "Another Way of using word";
+                UseMultipleWord multipleWordDetails = new UseMultipleWord();
+                multipleWordDetails = newWordDetails.useMultipleWords[0];
+                placeholderMultipleWords.text = multipleWordDetails.description;
+                
+                sentenceOfMultipleWords.text = multipleWordDetails.useMultipleWordSentences[0].description;
+            }
+    }
+
+    public void AntonymSetup()
+    {
+        if (allDetailData.newWords.Length == 1)
+            {
+
+                newWord.text = allDetailData.newWords[0].name;
+            
+                NewWord newWordDetails = new NewWord();
+                newWordDetails = allDetailData.newWords[0];
+                // title.text = "Another Way of using word";
+                Antonym antonymDetails = new Antonym();
+                antonymDetails = newWordDetails.antonyms[0];
+                antonymTitle.text = antonymDetails.description;
+                meaningAsAntonym.text = antonymDetails.meaning;
+                
+                sentenceOfAntonym.text = antonymDetails.antonymSentences[0].description;
+            }
+    }
+    public void SynonymSetup()
+    {
+        if (allDetailData.newWords.Length == 1)
+            {
+
+                newWord.text = allDetailData.newWords[0].name;
+            
+                NewWord newWordDetails = new NewWord();
+                newWordDetails = allDetailData.newWords[0];
+                // title.text = "Another Way of using word";
+                Synonym synonymDetails = new Synonym();
+                synonymDetails = newWordDetails.synonyms[0];
+                synonymTitle.text = synonymDetails.description;
+                meaningAsSynonym.text = synonymDetails.meaning;
+                
+                sentenceOfSynonym.text = synonymDetails.synonymSentences[0].description;
+            }
+    }
+
+    public void SetBottomTitleLabel()
+    {
+        GameObject bottomPanel = GameObject.Find("BottomPanel");
+        GameObject childObj = bottomPanel.transform.GetChild(0).gameObject;
+        TMPro.TMP_Text numberText = childObj.GetComponent<TMPro.TMP_Text>();
+        numberText.text = nextNumber + "/" + totalNumber;
+    }
+
+    public void NextButton(Button button)
+    {
+
+        if (button.tag == "Next")
+        {
+            nextNumber = nextNumber + 1;
+        }
+        else
+        {
+            if (nextNumber > 1)
+            {
+                nextNumber = nextNumber - 1;
+            }
+        }
+        var currentBoardNumber = nextNumber - 1;
+
+        for(int i = 0; i < diffBoards.Length; i++)
+        {
+            if (i != currentBoardNumber)
+            {
+                diffBoards[i].gameObject.SetActive(false);
+            }
+        }
+
+        diffBoards[currentBoardNumber].gameObject.SetActive(true);
+
+        Debug.Log(diffBoards[currentBoardNumber].gameObject.name);
+
+        SetBottomTitleLabel();
+
+        if (nextNumber == 1)
+        {
+            NewWordSetup();
+        }
+        else if (nextNumber == 2)
+        {
+            VerbSetup();
+        }
+        else if (nextNumber == 3)
+        {
+            AdverbSetup();
+        }
+        else if (nextNumber == 4)
+        {
+            AdjectiveSetup();
+        }
+        else if (nextNumber == 5)
+        {
+            ConversationSetup();
+        }
+        else if (nextNumber == 6)
+        {
+            DailyTipsSetup();
+        }
+        else if (nextNumber == 7)
+        {
+            AnotherWayOfUsingWordSetup();
+        }
+        else if (nextNumber == 8)
+        {
+            IdiomSetup();
+        }
+        else if (nextNumber == 9)
+        {
+            MultipleWordSetup();
+        }
+        else if (nextNumber == 10)
+        {
+            SynonymSetup();
+        }
+        else if (nextNumber == 11)
+        {
+            AntonymSetup();
         }
     }
+
+    
 
 }
